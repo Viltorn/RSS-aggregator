4990137dcd747abf3e8e2a8b055679e542ac42f6
Viltorn <viltorn@gmail.com>
2022-11-23 11:22:19 +0200

    add url validation

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### Hexlet tests and linter status:
-![Node CI](https://github.com/Viltorn/frontend-project-lvl2/actions/workflows/nodejs.yml/badge.svg)
+![Node CI](https://github.com/Viltorn/frontend-project-11/actions/workflows/nodejs.yml/badge.svg)
 [![Actions Status](https://github.com/Viltorn/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/Viltorn/frontend-project-11/actions)
 ### Codeclimate
 [![Maintainability](https://api.codeclimate.com/v1/badges/d00b54facb37c6439678/maintainability)](https://codeclimate.com/github/Viltorn/frontend-project-11/maintainability)
+
+### Link
+https://frontend-project-11-weld-five.vercel.app/
--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 import './styles.scss';
 import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import render from './view.js';
 
-console.log('Hello World!');
+console.log('HELLO');
+
+const initialState = {
+  form: {
+    valid: true,
+    errors: '',
+    url: '',
+  },
+  feeds: [],
+};
+
+const state = onChange(initialState, render());
+
+const schema = yup.string().required().url('incorrect url').notOneOf(state.feeds, 'Link already added');
+
+const btn = document.querySelector('button');
+const input = document.querySelector('#url-input');
+const form = document.querySelector('form');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { value } = input;
+  schema.validate(value)
+    .then(() => {
+      state.form.errors = '';
+      state.form.valid = true;
+      state.feeds.push(value);
+      form.reset();
+      input.focus();
+      console.log(state.feeds);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      const error = err.message;
+      state.form.errors = error;
+      state.form.valid = false;
+    });
+});
